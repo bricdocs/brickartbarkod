b5b462ae1102cf4e01d9eeff5c6236811587f164
bricdocs <bricdocs.web@gmail.com>
2026-08-15 22:31:14 +0300

    Comment out card mappings in JANNERSTEN_DECK_MAP
    Commented out several card mappings for clarity.

--- barkod-data.js
+++ barkod-data.js
@@ -6,45 +6,45 @@
 const JANNERSTEN_DECK_MAP = {
     // Sinek 4 Canlı Ekran Yakalaması
     "D-I-D-I-D-I-D-K-G": "Sinek 4 (4 of Clubs)",
-    "D-I-G-I-G-I-D-K-G": "Sinek 4 / Maça 4 (Düz Varyasyon)",//ESKI Örüntü: I-D-I-D-I-D-I-D-I
+    "D-I-G-I-G-I-D-K-G": "Maça 4",//ESKI Örüntü: I-D-I-D-I-D-I-D-I
     "D-I-D-I-D-I-G-K-D": "Diamond 3",
 
-    // Aslar
-    "I-D-I-D-K-D-I-D-I": "Sinek As (Ace of Clubs)",
-    "I-D-I-D-I-D-K-D-I": "Kupa As (Ace of Hearts)",
-    "I-D-I-D-I-D-I-D-K": "Karo As (Ace of Diamonds)",
-    "I-D-K-D-I-D-I-D-I": "Maça As (Ace of Spades)",
+    // // Aslar
+    // "I-D-I-D-K-D-I-D-I": "Sinek As (Ace of Clubs)",
+    // "I-D-I-D-I-D-K-D-I": "Kupa As (Ace of Hearts)",
+    // "I-D-I-D-I-D-I-D-K": "Karo As (Ace of Diamonds)",
+    // "I-D-K-D-I-D-I-D-I": "Maça As (Ace of Spades)",
 
-    // Valeler ve 3'lüler
-    "I-D-I-D-K-D-I-D-K": "Kupa Vale (Jack of Hearts)",
-    "I-G-I-G-K-G-I-G-K": "Kupa 3 (3 of Hearts)", 
-    "K-D-I-D-K-D-I-D-I": "Sinek Vale",
-    "I-D-K-D-I-D-K-D-I": "Maça Vale",
+    // // Valeler ve 3'lüler
+    // "I-D-I-D-K-D-I-D-K": "Kupa Vale (Jack of Hearts)",
+    // "I-G-I-G-K-G-I-G-K": "Kupa 3 (3 of Hearts)", 
+    // "K-D-I-D-K-D-I-D-I": "Sinek Vale",
+    // "I-D-K-D-I-D-K-D-I": "Maça Vale",
 
-    // Kızlar ve 6'lılar
-    "K-D-K-D-K-D-I-D-I": "Sinek Kız (Queen of Clubs)",
-    "K-G-K-G-K-G-I-G-I": "Sinek 6 (6 of Clubs)", 
-    "I-D-K-D-K-D-K-D-I": "Maça Kız (Queen of Spades)",
-    "I-G-K-G-K-G-K-G-I": "Maça 6 (6 of Spades)",
-    "I-D-I-D-K-D-K-D-K": "Kupa Kız",
+    // // Kızlar ve 6'lılar
+    // "K-D-K-D-K-D-I-D-I": "Sinek Kız (Queen of Clubs)",
+    // "K-G-K-G-K-G-I-G-I": "Sinek 6 (6 of Clubs)", 
+    // "I-D-K-D-K-D-K-D-I": "Maça Kız (Queen of Spades)",
+    // "I-G-K-G-K-G-K-G-I": "Maça 6 (6 of Spades)",
+    // "I-D-I-D-K-D-K-D-K": "Kupa Kız",
 
-    // Papazlar ve 2'liler
-    "K-D-I-D-I-D-I-D-I": "Sinek Papaz (King of Clubs)",
-    "K-G-I-G-I-G-I-G-I": "Sinek 2 (2 of Clubs)",
-    "I-D-I-D-I-D-K-D-I": "Karo Papaz (King of Diamonds)",
-    "I-G-I-G-I-G-K-G-I": "Karo 2 (2 of Diamonds)",
-    "I-D-K-D-I-D-I-D-I": "Maça Papaz",
-    "I-D-I-D-K-D-I-D-I": "Kupa Papaz",
+    // // Papazlar ve 2'liler
+    // "K-D-I-D-I-D-I-D-I": "Sinek Papaz (King of Clubs)",
+    // "K-G-I-G-I-G-I-G-I": "Sinek 2 (2 of Clubs)",
+    // "I-D-I-D-I-D-K-D-I": "Karo Papaz (King of Diamonds)",
+    // "I-G-I-G-I-G-K-G-I": "Karo 2 (2 of Diamonds)",
+    // "I-D-K-D-I-D-I-D-I": "Maça Papaz",
+    // "I-D-I-D-K-D-I-D-I": "Kupa Papaz",
 
-    // Diğer Sayı Kartları
-    "K-D-K-D-I-D-I-D-I": "Sinek 10 (10 of Clubs)",
-    "I-D-K-D-K-D-I-D-I": "Maça 10 (10 of Spades)",
-    "I-G-K-G-K-G-I-G-I": "Maça 9 (9 of Spades)",
-    "I-D-I-D-K-D-K-D-I": "Kupa 9 (9 of Hearts)",
-    "I-G-I-G-K-G-K-G-I": "Kupa 5 (5 of Hearts)",
-    "I-D-I-D-I-D-K-D-K": "Karo 10 (10 of Diamonds)",
-    "I-G-I-G-I-G-K-G-K": "Karo 5 / Karo 7 / Sinek 8", 
-    "K-D-K-D-K-D-K-D-I": "Sinek 7 (7 of Clubs)",
-    "I-D-I-D-I-D-I-D-K": "Kupa 4 (4 of Hearts)",
-    "K-D-I-D-I-D-I-D-I": "Karo 4 (4 of Diamonds)"
+    // // Diğer Sayı Kartları
+    // "K-D-K-D-I-D-I-D-I": "Sinek 10 (10 of Clubs)",
+    // "I-D-K-D-K-D-I-D-I": "Maça 10 (10 of Spades)",
+    // "I-G-K-G-K-G-I-G-I": "Maça 9 (9 of Spades)",
+    // "I-D-I-D-K-D-K-D-I": "Kupa 9 (9 of Hearts)",
+    // "I-G-I-G-K-G-K-G-I": "Kupa 5 (5 of Hearts)",
+    // "I-D-I-D-I-D-K-D-K": "Karo 10 (10 of Diamonds)",
+    // "I-G-I-G-I-G-K-G-K": "Karo 5 / Karo 7 / Sinek 8", 
+    // "K-D-K-D-K-D-K-D-I": "Sinek 7 (7 of Clubs)",
+    // "I-D-I-D-I-D-I-D-K": "Kupa 4 (4 of Hearts)",
+    // "K-D-I-D-I-D-I-D-I": "Karo 4 (4 of Diamonds)"
 };
